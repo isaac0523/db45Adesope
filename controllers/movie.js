ba637ab668d79e420d9047de415b3ebe484da397
isaac0523 <54419363+isaac0523@users.noreply.github.com>
2021-11-17 22:04:39 -0600

    View for deleting an instance of your collection.

--- controllers/movie.js
+++ controllers/movie.js
@@ -105,3 +105,49 @@ failed`);
         res.send(`{'error': '${err}'}`); 
     } 
 }; 
+
+//Handle building the view for creating a movie. 
+// No body, no in path parameter, no query. 
+// Does not need to be async 
+exports.movie_create_Page =  function(req, res) { 
+    console.log("create view") 
+    try{ 
+        res.render('moviecreate', { title: 'movie Create'}); 
+    } 
+    catch(err){ 
+        res.status(500) 
+        res.send(`{'error': '${err}'}`); 
+    } 
+}; 
+
+// Handle building the view for updating a movie. 
+// query provides the id 
+exports.movie_update_Page =  async function(req, res) { 
+    console.log("update view for item "+req.query.id) 
+    try{ 
+        let result = await Movie.findById(req.query.id) 
+        res.render('movieupdate', { title: 'movie Update', toShow: result }); 
+    } 
+    catch(err){ 
+        res.status(500) 
+        res.send(`{'error': '${err}'}`); 
+    } 
+}; 
+
+/ Handle a delete one view with id from query 
+exports.movie_delete_Page = async function(req, res) { 
+    console.log("Delete view for id "  + req.query.id) 
+    try{ 
+        result = await Movie.findById(req.query.id) 
+        res.render('moviedelete', { title: 'movie Delete', toShow: 
+result }); 
+    } 
+    catch(err){ 
+        res.status(500) 
+        res.send(`{'error': '${err}'}`); 
+    } 
+}; 
+
+
+
+
--- routes/resource.js
+++ routes/resource.js
@@ -32,5 +32,14 @@ router.get('/movies', movie_controller.movie_list);
 /* GET detail MOVIE page */ 
 router.get('/detail', movie_controller.movie_view_one_Page); 
  
+/* GET create movie page */ 
+router.get('/create', movie_controller.movie_create_Page); 
  
 module.exports = router; 
+
+/* GET create update page */ 
+router.get('/update', movie_controller.movie_update_Page); 
+
+/* GET create movie page */ 
+router.get('/delete', movie_controlers.movie_delete_Page); 
+ 
