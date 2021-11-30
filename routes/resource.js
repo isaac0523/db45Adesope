030c9ee20253dce22821ce142d2e561845805de0
isaac0523 <54419363+isaac0523@users.noreply.github.com>
2021-11-30 01:12:38 -0600

    authentication

--- routes/resource.js
+++ routes/resource.js
@@ -1,6 +1,16 @@
 var express = require('express'); 
 var router = express.Router(); 
  
+// A little function to check if we have an authorized user and continue on 
+// redirect to login. 
+const secured = (req, res, next) => { 
+    if (req.user){ 
+      return next(); 
+    } 
+    req.session.returnTo = req.originalUrl; 
+    res.redirect("/login"); 
+  } 
+
 // Require controller modules. 
 var api_controller = require('../controllers/api'); 
 var movie_controller = require('../controllers/movie'); 
@@ -35,11 +45,12 @@ router.get('/detail', movie_controller.movie_view_one_Page);
 /* GET create movie page */ 
 router.get('/create', movie_controller.movie_create_Page); 
  
-module.exports = router; 
-
 /* GET create update page */ 
-router.get('/update', movie_controller.movie_update_Page); 
+router.get('/update',secured, movie_controller.movie_update_Page); 
 
 /* GET create movie page */ 
 router.get('/delete', movie_controller.movie_delete_Page); 
+
+
+  module.exports = router; 
  
